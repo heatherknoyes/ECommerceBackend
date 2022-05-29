a8224ad325cfe7df87fd8820201d7a0a9121847d
Heather Noyes <heatherknoyes@gmail.com>
2022-05-28 23:52:56 -0400

    Updated various bug fixes on tags not returning on get all, categories not lining up correctly because of incorrect index.js

--- models/index.js
+++ models/index.js
@@ -11,14 +11,14 @@ Product.belongsTo(Category, {
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "id",
+  foreignKey: "category_id",
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
     model: ProductTag,
-    foreignKey: "tag_id",
+    foreignKey: "product_id",
   },
 });
 
@@ -26,6 +26,7 @@ Product.belongsToMany(Tag, {
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
+    foreignKey: "tag_id",
   },
 });
 
